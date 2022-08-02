2b96ab3459498d4a18b36c4cb924a5bdf42fbad6
shriyadxt <dikshtih.s@northeastern.edu>
2022-08-02 00:11:56 -0400

    adding restaurant variables

---
+++ frontend/src/index.js
@@ -0,0 +1,31 @@
+import Restaurent from '../models/restaurentModel.js'
+import asyncHandler from 'express-async-handler'
+
+
+const getRestaurents = asyncHandler(async (req, res) => {
+  const restaurents = await Restaurent.find({})
+  res.json(restaurents)
+})
+
+const getRestaurantById = asyncHandler(async (req, res) => {
+    const restaurant = await Restaurent.findById(req.params.id)
+  
+    if (restaurant) {
+      res.json({
+        _id: restaurant._id,
+        name: restaurant.name,
+        type: restaurant.type,
+        tables: restaurant.tables,
+        phoneNo: restaurant.phoneNo,
+        email: restaurant.email,
+        location: restaurant.location,
+        images: restaurant.images,
+        description: restaurant.description,
+      })
+  
+    } else {
+      res.status(404)
+      throw new Error('Restaurant not found')
+    }
+  
+  })
