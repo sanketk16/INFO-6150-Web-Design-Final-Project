5e9faf46b8ac334b1246e20074f33d0727d97124
shriyadxt <dikshtih.s@northeastern.edu>
2022-08-02 04:21:34 -0400

    creating the restaurents

--- frontend/src/actions/restaurantsActions.js
+++ frontend/src/actions/restaurantsActions.js
@@ -72,3 +72,111 @@ export const getRestDetails = (id) => async (dispatch, getState) => {
           })
     }
 }
+
+export const createRest = (name, type, tables, phoneNo, email, location, image1, image2, image3, description) => async (dispatch, getState) => {
+    try {
+          dispatch({
+                type: REST_CREATE_REQUEST,
+          })
+
+          //   const {
+          //     userLogin: { userInfo },
+          //   } = getState()
+
+          //   const config = {
+          //     headers: {
+          //       Authorization: `Bearer ${userInfo.token}`,
+          //     },
+          //   }
+
+          const { data } = await axios.post(`/api/restaurents/create`, { name, type, tables, phoneNo, email, location, image1, image2, image3, description })
+
+          dispatch({
+                type: REST_CREATE_SUCCESS,
+                payload: data,
+          })
+    } catch (error) {
+          dispatch({
+                type: REST_CREATE_FAIL,
+                payload:
+                      error.response && error.response.data.message
+                            ? error.response.data.message
+                            : error.message,
+          })
+    }
+}
+
+
+
+export const deleteRest = (id) => async (dispatch) => {
+    try {
+          dispatch({
+                type: REST_DELETE_REQUEST,
+          })
+
+          await axios.delete(`/api/restaurents/${id}`)
+
+          dispatch({
+                type: REST_DELETE_SUCCESS,
+
+          })
+    } catch (error) {
+          dispatch({
+                type: REST_DELETE_FAIL,
+                payload:
+                      error.response && error.response.data.message
+                            ? error.response.data.message
+                            : error.message,
+          })
+    }
+}
+
+
+
+export const updateRestaurantDetails = (restaurant) => async (dispatch) => {
+
+    try {
+          dispatch({
+                type: REST_UPDATE_REQUEST,
+          })
+
+          const { data } = axios.put(`/api/restaurents/${restaurant._id}`, restaurant)
+
+          dispatch({
+                type: REST_UPDATE_SUCCESS,
+                payload: data,
+          })
+
+    } catch (error) {
+          dispatch({
+                type: REST_UPDATE_FAIL,
+                payload:
+                      error.response && error.response.data.message
+                            ? error.response.data.message
+                            : error.message
+          })
+    }
+
+}
+
+
+export const filterRestaurants = (searchkey,type) => async dispatch => {
+
+    var filteredRest;
+
+    dispatch({ type: REST_ALL_REQUEST })
+
+    try {
+          const response = await axios.get('/api/restaurents/')
+          filteredRest = response.data.filter(restaurant => restaurant.name.toLowerCase().includes(searchkey))
+
+          if (type != 'all') {
+                filteredRest = response.data.filter(restaurant => restaurant.type.toLowerCase()==type)
+          }
+
+          dispatch({ type: REST_ALL_SUCCESS, payload: filteredRest })
+    } catch (error) {
+          dispatch({ type: REST_ALL_FAIL, payload: error })
+    }
+} 
+
