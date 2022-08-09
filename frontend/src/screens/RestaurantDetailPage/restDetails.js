20921eaa678970f36a772b47c7697bb40e2b8a74
sanketk16 <khadke.s@northeastern.edu>
2022-08-09 19:11:21 -0400

    css change

--- frontend/src/screens/RestaurantDetailPage/restDetails.js
+++ frontend/src/screens/RestaurantDetailPage/restDetails.js
@@ -67,24 +67,21 @@ const RestaurantDetail = ({ match, history }) => {
                                         <br></br>
                                         <div class="vl">
                                             <div className="title">
-                                                <h6>Grand Hotel</h6>
+                                                <h6>Bistro Hotels</h6>
                                                 <h1> {restaurants.name}</h1>
                                             </div>
                                         </div>
                                         <hr></hr>
 
                                         <div className="about">
-                                            <h6>The authentic Chinese restaurant in the city </h6>
-                                            <hr></hr>
-
-                                            <p>Grand Hotel’s signature Chinese restaurant Shang Palace celebrates cuisine steeped in rich tradition. The décor is inspired from Beijing’s traditional siheyuan houses and blends beautifully with modern luxury. </p>
-                                            <p>At Shang Palace we invite you to enjoy the varied flavours of Dongbei, Sichuan and Canton. Succulent meats, fresh seafood, delicate dim sum, a range of Chinese teas and cocktails all feature in our extensive menu, allowing you to indulge in a true feast.</p>
-                                            <p>Our weekday lunch set menu features intricate delicacies including flavourful soups, the finest dim sum, meats, seafood, rice and noodles. Available from Tuesday to Friday‬.</p>
-                                            <p>Location - Level 3</p>
-                                            <p>Lunch - 12 noon to 3pm, Monday to Sunday</p>
-                                            <p>Dinner - 6.30pm to 11pm, Monday to Sunday</p>
-                                            <p>Weekend Yum Cha Lunch - 12noon to 3pm, Saturday and Sunday</p>
-                                            <p>{restaurants.description}</p>
+                                            
+                                            <p>Location: {restaurants.location}</p>
+                                            <br></br>
+                                            <p>Lunch Timings: 12 noon to 3pm, Monday to Sunday</p>
+                                            <br></br>
+                                            <p>Dinner Timings: 6.00 pm to 10.00 pm, Monday to Sunday</p>
+                                            <br></br>
+                                            <p>Description: {restaurants.description}</p>
                                         </div>
 
                                         <Row>
@@ -98,10 +95,6 @@ const RestaurantDetail = ({ match, history }) => {
 
                                     <Col className="de">
 
-                                        {/* <div class="d-grid gap-2">
-                                            <a class="btnn" type="button" href="/booknow">Book Your Table Now!</a>
-                                        </div> */}
-
                                         <ListGroup.Item>
                                             <Button
                                                 onClick={bookNowHandler}
@@ -109,7 +102,7 @@ const RestaurantDetail = ({ match, history }) => {
                                                 type='button'
                                                 
                                             >
-                                               Book Now !
+                                               Book A Seat
                                             </Button>
                                         </ListGroup.Item>
 
@@ -137,34 +130,12 @@ const RestaurantDetail = ({ match, history }) => {
                                                 <div className="title">Location
                                                     <p >{restaurants.location}</p>
                                                 </div>
-                                                <div className="title">Opening Hours
-                                                    <p >Breakfast: 8.00a.m to 10.30a.m</p>
-                                                    <p >Lunch: 11.30a.m to 4.30p.m</p>
-                                                    <p >Dinner: 5.00p.m to 11.30p.m </p>
-                                                    <br></br>
-                                                    <p >Weekend Yum Cha Saturday
-                                                        and Sunday 12pm - 3pm</p>
-
-
-                                                </div>
-                                                <div className="title">Location
-                                                    <p >{restaurants.location}</p>
-                                                </div>
-                                                <div className="title">Dress Code
-                                                    <p >Smart Casual</p>
-                                                </div>
-
-                                                <div className="title">Smoking Policy
-                                                    <p >No smoking permitted</p>
-                                                </div>
-
                                             </Form>
 
 
 
 
                                         </Row>
-                                        <hr style={{ color: "#da9100", height: "5px", width: "50%" }}></hr>
 
 
 
