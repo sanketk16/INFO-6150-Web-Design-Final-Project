4e8d81c87d9b1b505d38f9e103029f37f6502af1
harshita0729 <99058341+harshita0729@users.noreply.github.com>
2022-08-10 14:16:04 -0400

    registerUser function edited

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,14 +25,14 @@ const authUser = asyncHandler(async(req, res) => {
 const registerUser = asyncHandler(async(req, res) => {
     const { name, email, password } = req.body
 
-   const userExists = await User.findOne({ email })
+   const userExists = User.findOne({ email })
 
    if(userExists) {
        res.status(400)
        throw new Error('User already exists')
    }
 
-   const user = await User.create({
+   const user = User.create({
        name,
        email,
        password
