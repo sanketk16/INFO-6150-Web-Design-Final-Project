6f0738a91c14bf0343d5c6c1c53eba21d7ba2bc1
harshita0729 <99058341+harshita0729@users.noreply.github.com>
2022-08-10 14:27:22 -0400

    error message edited

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,7 +29,7 @@ const registerUser = asyncHandler(async(req, res) => {
 
    if(userExists) {
        res.status(400)
-       throw new Error('User already exists')
+       throw new Error('Per-existing user')
    }
 
    const user = await User.create({
