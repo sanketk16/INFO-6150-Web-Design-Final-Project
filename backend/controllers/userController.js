70073d9bd797d994a8371753c40119ee9bc0249b
harshita0729 <99058341+harshita0729@users.noreply.github.com>
2022-08-10 14:22:37 -0400

    async added to authUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateTokens.js'
 import User from '../models/userModel.js'
 
-const authUser = asyncHandler((req, res) => {
+const authUser = asyncHandler(async(req, res) => {
     const { email, password } = req.body
 
    const user = await User.findOne({ email })
