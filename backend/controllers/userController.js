f63374821cf040744e28a7b4464b250b296c9e62
harshita0729 <99058341+harshita0729@users.noreply.github.com>
2022-08-10 14:23:47 -0400

    async added to registerUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,7 +22,7 @@ const authUser = asyncHandler(async(req, res) => {
 
 })
 
-const registerUser = asyncHandler((req, res) => {
+const registerUser = asyncHandler(async(req, res) => {
     const { name, email, password } = req.body
 
    const userExists = await User.findOne({ email })
